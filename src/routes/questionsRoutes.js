d8e308dab102bf39c0c452138f508154ef19679d
higorcashme <higorasilverio@outlook.com>
2021-09-15 22:07:37 -0300

    ♻️ routes: unused logs removed

--- src/routes/questionsRoutes.js
+++ src/routes/questionsRoutes.js
@@ -11,12 +11,10 @@ const questionsRoutes = (app) => {
     .route("/api/questions")
     .post((req, res, next) => {
       //middleware
-      console.log(`${req.method} request from: ${req.originalUrl}`);
       next();
     }, addNewQuestion)
     .get((req, res, next) => {
       //middleware
-      console.log(`${req.method} request from: ${req.originalUrl}`);
       next();
     }, getQuestions);
 
@@ -24,17 +22,14 @@ const questionsRoutes = (app) => {
     .route("/api/questions/:questionId")
     .get((req, res, next) => {
       //middleware
-      console.log(`${req.method} request from: ${req.originalUrl}`);
       next();
     }, getQuestionWithId)
     .patch((req, res, next) => {
       //middleware
-      console.log(`${req.method} request from: ${req.originalUrl}`);
       next();
     }, updateQuestion)
     .delete((req, res, next) => {
       //middleware
-      console.log(`${req.method} request from: ${req.originalUrl}`);
       next();
     }, deleteQuestion);
 };
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -7,19 +7,16 @@ import {
 const usersRoutes = (app) => {
   app.route("/api/users/register").post((req, res, next) => {
     //middleware
-    console.log(`${req.method} request from: ${req.originalUrl}`);
     next();
   }, registerUser);
 
   app.route("/api/users/:userId").get((req, res, next) => {
     //middleware
-    console.log(`${req.method} request from: ${req.originalUrl}`);
     next();
   }, getUserWithId);
 
   app.route("/api/users/change-role/:userId").patch((req, res, next) => {
     //middleware
-    console.log(`${req.method} request from: ${req.originalUrl}`);
     next();
   }, updateUserRole);
 };
